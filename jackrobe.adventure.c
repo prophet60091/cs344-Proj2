baa80eb2d95c0daff972a1a3f3af0c251e91772e
Robert Jackson <jackrobe@onid.oregonestate.edu>
2016-05-01 17:04:04 -0600

    rewritten, to random slect 7 of 10 files changed file names for ease of reading read function works to an extent.

--- jackrobe.adventure.c
+++ jackrobe.adventure.c
@@ -9,15 +9,28 @@
 #include <sys/unistd.h>
 #include <time.h>
 #include <sys/stat.h>
+#include <assert.h>
+
+# ifndef TYPE
+# define TYPE      void*
+# endif
+typedef struct Room Room;
 
 #define ARYSZ(x)  ( (sizeof(x) / sizeof((x)[0])) )
 
 char NAME[10];
 const int  MINROOMX = 3; // minimum number of room connections
-const int MAXCONNECTIONS = 6;
-char *rooms_str[]={ "Dumbeldor's Office", "Room of Requirement", "Great Hall", "Potions", "Divination", "Hospital", "Herbology" };
-const char *rooms_indi[]={"ROOM NAME:", "ROOM TYPE:", "CONNECTION:"};
+const int MAXCONNECTIONS = 6; // maximum number of connections per room
+const int MAXROOMS = 7; // max number of rooms in play
+char *rooms_str[]={ "Dumbeldor's_Office", "Room_of_Requirement", "Great_Hall", "Potions", "Divination", "Hospital", "Herbology", "Owlry", "Gryffendor_Commons", "Slytherin_Commons" };
+char *roomsTypes_str[]={ "MID_ROOM", "END_ROOM", "START_ROOM"};
+
+int inPlay[10]={0,1,2,3,4,5,6,7,8,9}; // arry to track which rooms are in play
+
 char dirName[] = "./Jackrobe.Rooms.";
+int GAMEOVER =0;
+
+
 
 //error function
 void error(char *msg)
@@ -26,32 +39,50 @@ void error(char *msg)
     exit(1);
 }
 
-int get_user(){
-
-    char ans[2];
-    while(1){
-        printf("Enter user name (10 chars max): ");
-
-        fgets(NAME, 10, stdin);
-        printf("Hi %s", NAME);
-
-
-        if(!strchr(NAME, '\n'))
-            while(fgetc(stdin)!='\n');//discard until newline
-
-        printf("\nIs this correct? enter y or n: ");
-        fgets(ans, 2, stdin);
-
-        if(!strchr(ans, '\n'))
-            while(fgetc(stdin)!='\n');//discard until newline
-
-        if((strcmp(ans, "y") == 0 )|| (strcmp(ans, "Y") == 0)){
-            return 0;
-        }else{
-            printf("Try Again - ");
+//#################### Shuffles an Array
+// @param  a pointer to an array
+// @param number of elements in the array
+void array_shuffle(int *a, int size){
+    if (size > 1)
+    {
+        int i;
+        for (i = 0; i < rand()% 100 ; i++) {
+            int sp = rand() % 10; // start point for shuffle
+            int sw = rand() % 10; // swap point for shuffle
+            int t = a[sp]; // hold one
+            a[sp] = a[sw];
+            a[sw] = t;
         }
     }
 }
+
+//
+//int get_user(){
+//
+//    char ans[2];
+//    while(1){
+//        printf("Enter user name (10 chars max): ");
+//
+//        fgets(NAME, 10, stdin);
+//        printf("Hi %s", NAME);
+//
+//
+//        if(!strchr(NAME, '\n'))
+//            while(fgetc(stdin)!='\n');//discard until newline
+//
+//        printf("\nIs this correct? enter y or n: ");
+//        fgets(ans, 2, stdin);
+//
+//        if(!strchr(ans, '\n'))
+//            while(fgetc(stdin)!='\n');//discard until newline
+//
+//        if((strcmp(ans, "y") == 0 )|| (strcmp(ans, "Y") == 0)){
+//            return 0;
+//        }else{
+//            printf("Try Again - ");
+//        }
+//    }
+//}
 //int add_to_file(char * txt, FILE * fp){
 //
 //    if(fopen(fp, "a") < 0)
@@ -64,24 +95,30 @@ int get_user(){
 //    return 0;
 //}
 
-
-
-
+//#################### OPENS FILES
+// @param  the directory
+// @param the file name
+// @param the action <a, w, r, etc...>
+// Returns 0 on success
 FILE * open_file(char * dir, char * fileName, char * action){
     char file[50];
     //dir name and name of file
-    snprintf(file, 50, "%s/%s.txt", dir, fileName);
+    snprintf(file, 50, "%s/%s", dir, fileName);
 
     //open the newfile for writing
     // with the ROOM: + Room Name
     FILE *fp;
 
-    if((fp = fopen(file , action )) < 0)
-        error("Couldn't create File");;
-
-    return fp;
+    if((fp = fopen(file , action )) < 0) {
+        error("Couldn't open file " );;
+    }else{
+        return fp;
+    }
 }
 
+//todo Ideally combine the file generation inside one function to minimize open/closes
+//#################### GENERATE FILES FOR GAME
+// Returns 0 on success
 int gen_files(){
 
     int i;
@@ -97,51 +134,58 @@ int gen_files(){
         mkdir(dirName, 0700);
     }
 
-    int nRooms = ARYSZ(rooms_str); // the number of rooms in my array
-    for( i= 0; i < nRooms; i++){
+    int nRooms = ARYSZ(rooms_str); // the number of rooms
+    int * arryP = inPlay;
+    array_shuffle(arryP, nRooms); // reshuffle and use only the first 7 from here on out
+    FILE * fp;
+
+    for( i= 0; i < MAXROOMS; i++){
 
         char fLine[50];
-        FILE * fp = open_file(dirName, rooms_str[i], "w");
+
+        fp = open_file(dirName, rooms_str[inPlay[i]], "w");
 
         //make the first line
         //write the name of a room to it
-        if(fprintf(fp, "ROOM NAME: %s\n", rooms_str[i]) < 0)
+        if(fprintf(fp, "ROOM NAME: %s\n", rooms_str[inPlay[i]]) < 0)
             error("Couldn't write to file");
 
         fclose(fp);
     }
     return(0);
 }
-
+//#################### GENERATE ROOM'S CONNECTIONS
+//creates a matrix then writes that matrix
+// Returns 0 on success
 int gen_connections(){
+
     int i, j;
-    int nRooms = ARYSZ(rooms_str); // the number of rooms in my array
-    int conxMatrix[nRooms][nRooms]; // a matrix of connected rooms
+    int conxMatrix[MAXROOMS][MAXROOMS]; // a matrix of connected rooms
     memset(conxMatrix, 0, sizeof(conxMatrix));
-    int cxCount[nRooms]; // number of connections in a room
+
+    int cxCount[MAXROOMS]; // number of connections in a room
     memset(cxCount, 0, sizeof(cxCount));
 
-    FILE *curFile;
     FILE * ConnectFile;
 
-    //cycle though the files
-    for( i= 0; i < nRooms; i++) {
-        // set the number of connection to be made
-        srand(time(NULL));
+    //cycle over the files
+    for( i= 0; i < MAXROOMS; i++) {
+
         int numCx = rand() % 4 + MINROOMX; // number of connections to be added to each room
 
-        if(cxCount[i] <= MINROOMX ) { // no reason to add more connections than necessary
+        // no reason to add more connections than necessary
+        if(cxCount[i] <= MINROOMX ) {
 
-            //make a random number of connections for the room
-            while (cxCount[i] < numCx) {
+            while (cxCount[i] < numCx) {//make a random number of connections for the room
 //
-                int conx = rand() % nRooms;
+                int conx = rand() % MAXROOMS;
                 //dont connect if the connecting room as too many conection or the room is the same
                 if (cxCount[conx] < MAXCONNECTIONS && conx != i){
 
-                    if ((conxMatrix[i][conx] == 0) && (conxMatrix[conx][i] == 0) ) {
+                    if ((conxMatrix[i][conx] == 0) && (conxMatrix[conx][i] == 0) ) { //only change if it hasn't already been
                         conxMatrix[i][conx] = 1;
                         conxMatrix[conx][i] = 1;
+                        //increase the count of connections for both
                         cxCount[i]++;
                         cxCount[conx]++;
                     }
@@ -149,58 +193,117 @@ int gen_connections(){
             }
         }
     }
+    // Write info to the files
+    for(i=0; i < MAXROOMS; i++){
 
-    for(i=0; i < nRooms; i++){
-        ConnectFile = open_file(dirName, rooms_str[i], "a+"); // opening the file
-
+        ConnectFile = open_file(dirName, rooms_str[inPlay[i]], "a");// opening the file
         int count =1;
-        for(j=0; j < nRooms; j++) {
+        for(j=0; j < MAXROOMS; j++) {
             if ((conxMatrix[i][j] == 1)) {
-                fprintf(ConnectFile, "CONNECTION %i: %s\n", count, rooms_str[j]);
+                fprintf(ConnectFile, "CONNECTION %i: %s\n", count, rooms_str[inPlay[j]]);
                 count++;
             }
-            printf("%i ", conxMatrix[i][j]);
+            //printf("%i ", conxMatrix[i][j]);
         }
-        printf("\n");
+        //printf("\n");
         close(ConnectFile);
     }
-
     return 0;
 }
 
+//#################### GENERATE ROOM TYPES FOR EXISTING ROOMS
+// Returns 0 on success
 int gen_room_type(){
-    int i, room1, room2, nRooms;
-    FILE * fp;
-    nRooms = ARYSZ(rooms_str);
+    /*todo make this happen in the gen files, only it requires the ability to read a file if done that way */
 
-    room1 = rand() % nRooms;
-    room2 = rand() % nRooms;
+    int i, room1, room2;
+    FILE * fp;
+    room1 = rand() % MAXROOMS;
+    room2 = rand() % MAXROOMS;
     //make sure we get two differnt ones
     while(room1 == room2){
-        room2 = rand() % nRooms;
+        room2 = rand() % MAXROOMS;
     }
 
-    fp = open_file(dirName, rooms_str[room1], "a+"); // opening the file
-        fprintf(fp, "ROOM TYPE: START_ROOM\n");
+    fp = open_file(dirName, rooms_str[inPlay[room1]], "a"); // opening the file
+        //error("can't open file for start room");
+
+    fprintf(fp, "ROOM TYPE: START_ROOM\n");
     close(fp);
 
-    fp = open_file(dirName, rooms_str[room2], "a+"); // opening the file
+    fp = open_file(dirName, rooms_str[inPlay[room2]], "a"); // opening the file
+        //error("can't open file for end room");
+
     fprintf(fp, "ROOM TYPE: END_ROOM\n");
     close(fp);
 
-    for(i=0; i < nRooms; i++){
+    // Add the defualt midroom to the remainder
+    for(i=0; i < MAXROOMS; i++){
         if ((i != room1) && (i != room2)){
-            fp = open_file(dirName, rooms_str[i], "a+"); // opening the file
+            fp = open_file(dirName, rooms_str[inPlay[i]], "a");
+                //error("cant open files for room types mid_room"); // opening the file
             fprintf(fp, "ROOM TYPE: MID_ROOM\n");
             close(fp);
         }
-
     }
+    return 0;
+}
+
+int find_start_room(){
+
+
+}
+
+
+int read_file(){
+    FILE * fp;
+    fp = open_file("D://CS//cs344//Proj2", "test", "r+");
+
+    char * action[50];
+    char * connections[50]={};
+    char * content[50];
+    char * ROOMNAME;
+    int tCount =0;
+    int count =0;
+
+    while( fscanf(fp, "%*s %*s %s",  content ) != EOF){
+        tCount++;
+    }
+    rewind(fp);
+
+    while( fscanf(fp, "%*s %*s %s\n",  content ) != EOF){
+
+        if(count == 0){
+           printf("CURRENT LOCATION:%s\n", content);
+
+
+        }else if((count >= 1) && (count < tCount-1)){
+
+            strcat(connections, content);
+            strcat(connections, ", ");
+
+        }else{
+
+            if ( strcmp(content, "END_ROOM") ==  0){
+
+                GAMEOVER=1;
+                printf("Game over!");
+            }
+
+        }
+
+        count++;
+    }
+
+    printf("POSSIBLE CONNECTIONS: %s", connections);
+
+    close(fp);
 
 }
 
 int main(int argc, char *argv[]) {
 
+    srand(time(NULL));
 //    //get_user();
     if(gen_files()){
         error("check");
@@ -218,5 +321,15 @@ int main(int argc, char *argv[]) {
 //        int conx = rand() % nRooms;
 //        printf("NUM CX\tROOM#\n%i\t\t%i\n", numCx,conx );
 //    }
+
+//int array[10]={0,1,2,3,4,5,6,7,8,9};
+//    int i;
+//    int * p = inPlay;
+//    array_shuffle(p, 10);
+//    for(i=0; i < 10; i ++){
+//        printf("%i ", inPlay[i]);
+//    }
+
+exit(0);
 }
 
